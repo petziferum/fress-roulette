a5c535991d30543c068672ac177051fd864fc949
Petzi <daboarderpjb@gmail.com>
2022-12-15 19:41:00 +0100

    initial commit working

---
+++ src/components/ReceipeCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card elevation="3">
+    <v-card-title>rezept {{ title }}</v-card-title>
+    <v-card-text>{{ recipe}}</v-card-text>
+  </v-card>
+</template>
+
+<script lang=ts setup>
+interface Recipe {
+  recipeName: string
+}
+
+defineProps({
+  title: { type: String, default: "Der Standard Titel" },
+  recipe: String,
+});
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        geladen? {{state.recipesLoaded}}
+        <v-btn @click="fetchRecipes">Lade Rezepte</v-btn>
+      </v-col>
+    </v-row>
+    <v-row justify="center" v-if="state.recipesLoaded">
+      <v-col cols="6" v-for="recipe of state.recipesList" :key="recipe.id">
+        <receipe-card :recipe="recipe" :title="recipe.recipeName" />
+     </v-col>
+
+    </v-row>
+    <div v-else>nichts geladen...</div>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import ReceipeCard from "@/components/ReceipeCard.vue";
+import { getCollection } from "@/plugins/firebase";
+import {reactive} from "vue";
+
+//Interface für den State
+interface State {
+  recipesLoaded: boolean,
+  recipesList: any[]
+}
+
+//Der State mit Typdeclaration aus dem Interface
+const state: State = reactive({
+ recipesLoaded: false,
+ recipesList: []
+});
+
+function fetchRecipes(): void {
+  console.info("fetch")
+  state.recipesList = [];
+  getCollection().then((res) => {
+    state.recipesLoaded = true;
+    res.forEach((doc) => {
+      console.info("r", doc.data());
+      state.recipesList.push(doc.data());
+    })
+  })
+}
+
+</script>
