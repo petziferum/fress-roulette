0d3b0aeee98372a0092fc7cd5e0ae4f78a4fd1f7
Petzi <daboarderpjb@gmail.com>
2022-12-19 16:44:21 +0100

    validation

--- src/components/user/MainViewUserDashboard.vue
+++ src/components/user/MainViewUserDashboard.vue
@@ -3,9 +3,12 @@
     <v-col cols="6">
       <v-card elevation="6" color="secondary">
         <v-card-subtitle class="pa-5"
-          >{{ userName }} - <v-btn @click="login" outline>login</v-btn>
-          <v-btn class="mx-2" @click="logout">logout</v-btn></v-card-subtitle
+          >{{ userName }} - <v-btn @click="login" variant="tonal">login</v-btn>
+          <v-btn class="mx-2" @click="logout" variant="tonal"
+            >logout</v-btn
+          ></v-card-subtitle
         >
+        <v-card-subtitle v-if="alert">{{ alertMessage }}</v-card-subtitle>
         <v-row>
           <v-col cols="4">
             <v-list>
@@ -16,11 +19,22 @@
             </v-list>
           </v-col>
           <v-col cols="8">
-            <div v-if="!loggedIn">
+            <div v-if="!loggedIn" style="display: flex; flex-direction: row">
               <v-text-field
+                ref="passField"
+                :rules="[required]"
+                variant="outlined"
                 label="passwort"
-                @click="addUsername"
+                v-model="password"
               ></v-text-field>
+              <v-btn
+                rounded="0"
+                flat
+                min-height="55"
+                @click="login"
+                variant="tonal"
+                >login</v-btn
+              >
             </div>
             <div v-else>
               <v-alert height="50%">angemeldet als {{ fu }}</v-alert>
@@ -38,23 +52,37 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import {
   getAuth,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-  signOut
+  signOut,
 } from "firebase/auth";
-
 
 // Todo: Typing ref Values
 const userName = ref<string | null>("Offline");
 const loggedIn = ref(false);
 const auth = getAuth();
-let fu = ref<string|null>(null);
+const password = ref("");
+const alert = ref(false);
+const alertMessage = ref<string | null>(null);
+let fu = ref<string | null>(null);
+const passField = ref();
+const required = computed(() => {
+  return (v: string) => !!v || "Darf nicht leer sein";
+});
 
-function login() {
-  signInWithEmailAndPassword(auth, "petziferum@gmail.com", "testtest")
+async function login() {
+  const valid = await passField.value.validate();
+  console.log("valid?", valid[0]);
+  if (!valid[0]) {
+    console.log("Valide");
+  } else {
+    console.log("Nicht valide");
+  }
+  /*
+  signInWithEmailAndPassword(auth, "petziferum@gmail.com", password.value) //testtest
     .then((credentials) => {
       const fireUser = credentials.user;
       fu.value = fireUser.email;
@@ -64,7 +92,10 @@ function login() {
     })
     .catch((error) => {
       console.log("error", error);
+      alert.value = true;
+      alertMessage.value = "Falsches Passowrt  -  " + error.toString();
     });
+   */
 }
 
 const logout = () => {
