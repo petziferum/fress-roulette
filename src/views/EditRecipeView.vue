f8f3c8f587b41cac4d94d1a0ab97a94eaaa932d7
Petzi <daboarderpjb@gmail.com>
2023-01-10 12:27:25 +0100

    dreck entfernen

--- src/views/EditRecipeView.vue
+++ src/views/EditRecipeView.vue
@@ -80,60 +80,6 @@
                   </td>
                 </tr>
               </v-table>
-              <!--<v-list>
-                <v-row>
-                  <v-col cols="4"><b>Menge</b></v-col>
-                  <v-col cols="8"><b>Zutat</b></v-col>
-                </v-row>
-                <v-divider></v-divider>
-                <transition-group name="slide">
-                  <template
-                    v-for="(item, i) in recipe.ingredients"
-                    :key="item.nr"
-                  >
-                    <v-divider v-if="i !== 0" :key="`${i}-divider`"></v-divider>
-                    <v-list-item class="slide">
-                      <v-list-item-title>
-                        <v-row>
-                          <v-col cols="3">
-                            {{ item.menge }}
-                          </v-col>
-                          <v-col cols="9">
-                            <template v-if="i == editItem">
-                              <v-form
-                                ref="editIngredient"
-                                @submit.prevent="editItem = null"
-                              >
-                                <v-text-field
-                                  label="Name"
-                                  v-model="recipe.ingredients[i].name"
-                                  @click:append.prevent="editItem = null"
-                                  append-icon="mdi-content-save"
-                                ></v-text-field>
-                              </v-form>
-                              <v-btn x-small color="red" @click="deleteItem(i)"
-                                >delete
-                              </v-btn>
-                            </template>
-                            <template v-else>
-                              {{ item.name }}
-                              <div class="float-right" v-if="editMode">
-                                <v-btn
-                                  class="mr-1"
-                                  x-small
-                                  @click="editItem = i"
-                                  >edit
-                                </v-btn>
-                              </div>
-                            </template>
-                          </v-col>
-                        </v-row>
-                      </v-list-item-title>
-                    </v-list-item>
-                  </template>
-                </transition-group>
-              </v-list>
-              -->
             </v-col>
             <v-col cols="12" md="6">
               <v-form @submit.prevent="addIngredient" ref="zutatForm">
