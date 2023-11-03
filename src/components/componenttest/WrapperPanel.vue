a64a7ca98890b186da83ecfd4483f4f0cf72e705
petziferum <daboarderpjb@gmail.com>
2023-11-03 18:03:10 +0100

    wrapper panels

--- src/components/componenttest/WrapperPanel.vue
+++ src/components/componenttest/WrapperPanel.vue
@@ -1,12 +1,10 @@
 <template>
-  <v-expansion-panels model-value="0">
     <v-expansion-panel>
       <v-expansion-panel-title ripple>{{ title }}</v-expansion-panel-title>
       <v-expansion-panel-text>
         <slot></slot>
       </v-expansion-panel-text>
     </v-expansion-panel>
-  </v-expansion-panels>
 </template>
 <script setup lang="ts">
 defineProps(["title"]);
