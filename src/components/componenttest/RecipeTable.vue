596f39e92f575a584a6dfedfb41aa5e6ded02191
petziferum <daboarderpjb@gmail.com>
2023-11-04 18:09:24 +0100

    Difficulty to description

--- src/components/componenttest/RecipeTable.vue
+++ src/components/componenttest/RecipeTable.vue
@@ -2,24 +2,24 @@
   <v-container>
     <v-autocomplete :items="difficulty" item-title="value" label="Schwierigkeit"/>
   </v-container>
-  <v-table>
+  <v-table class="elevation-8">
     <thead>
       <tr>
-        <th>Name</th>
-        <th>Schwierigkeit</th>
+        <th><b>Name</b></th>
+        <th><b>Schwierigkeit</b></th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="item in items" :key="item.name">
         <td>{{ item.recipeName }}</td>
-        <td>{{ item.difficulty }}</td>
+        <td>{{ DifficultyToDescription.get(item.difficulty) }}</td>
       </tr>
     </tbody></v-table
   >
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-import { DifficultyToRecord } from "@/components/Models/Difficulty";
+import { DifficultyToRecord, DifficultyToDescription } from "@/components/Models/Difficulty";
 defineProps({items: Array});
 const difficulty = ref(DifficultyToRecord);
 </script>
