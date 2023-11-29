0f7aacfc85040bb5150d4d1b443ec985b61ab275
petziferum <daboarderpjb@gmail.com>
2023-11-29 11:18:15 +0100

    create delet dialog

---
+++ src/components/DeletButtonDialog.vue
@@ -0,0 +1,27 @@
+<template>
+<v-dialog v-model="isOpen">
+  <v-template v-slot:activator="{ on }">
+    <v-btn v-on="on">Open Dialog</v-btn>
+  </v-template>
+  <v-card>
+    <v-card-title>Möchtest du das Rezept löschen?</v-card-title>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn variant="text" color="red" @click="cancel">abbrechen</v-btn>
+      <v-btn variant="text" color="green" type="submit">weiter</v-btn>
+  </v-card>
+</v-dialog>
+</template>
+<script setup lang="ts">
+
+import { ref } from "vue";
+
+const isOpen = ref(false);
+
+function cancel() {
+  isOpen.value = false;
+}
+</script>
+<style scoped>
+
+</style>
