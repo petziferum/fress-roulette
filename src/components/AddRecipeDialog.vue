b138c0e9b36efddecc5140369a4c0b3c6f0f419b
petziferum <daboarderpjb@gmail.com>
2024-01-03 19:18:28 +0100

    button rainbow

--- src/components/AddRecipeDialog.vue
+++ src/components/AddRecipeDialog.vue
@@ -1,9 +1,7 @@
 <template>
   <v-dialog v-model="isOpen">
-    <template v-slot:activator="{ props }">
-      <v-btn class="dialog" elevation="4" v-bind="props" large
-        ><v-icon>mdi-plus</v-icon></v-btn
-      >
+    <template v-slot:activator>
+      <div class="rainbowbutton"></div>
     </template>
     <v-card v-if="!loading">
       <v-card-title>Möchtest du ein neues Rezept erstellen?</v-card-title>
@@ -68,6 +66,56 @@ function createRecipe() {
 </script>
 
 <style scoped>
+.rainbowbutton {
+  height: 50px;
+  width: 100px;
+  position: relative;
+  transform: translate(-150%, -50%);
+  top: 50%;
+  left: 10%;
+  border-radius: 5px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+  &:before {
+    content: "";
+    height: 230%;
+    width: 200%;
+    position: absolute;
+    background: conic-gradient(
+      #fd004c,
+      #fe9000,
+      #fff020,
+      #3edf4b,
+      #3363ff,
+      #b102b7,
+      #fd004c
+    );
+    left: -50%;
+    top: -60%;
+    animation: spin 3s infinite linear;
+  }
+  &:after {
+    content: "Neu";
+    position: absolute;
+    cursor: pointer;
+    background-color: #1c1b29;
+    height: 93%;
+    width: 92%;
+    top: 3.5%;
+    left: 3.5%;
+    border-radius: 5px;
+    color: #ffffff;
+    font-size: 30px;
+    letter-spacing: 6px;
+    display: grid;
+    place-items: center;
+  }
+}
+@keyframes spin {
+  100% {
+    transform: rotate(-360deg);
+  }
+}
 .dialog {
   z-index: 2;
   top: 9px;
