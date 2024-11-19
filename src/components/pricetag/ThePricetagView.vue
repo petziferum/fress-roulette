5588adcc87e1b4f6bd6463be286c3ec048aa526c
petziferum <daboarderpjb@gmail.com>
2024-11-19 21:03:25 +0100

    date picker

--- src/components/pricetag/ThePricetagView.vue
+++ src/components/pricetag/ThePricetagView.vue
@@ -128,16 +128,6 @@ const pricetagEntry = reactive({
 });
 const entries = ref([]);
 
-const formattedDate = computed(() => {
-  return date.value.toLocaleString("de-DE", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-});
-
 async function findProductname() {
   if (!productname.value) return;
   // Firebase query to find matching product names
